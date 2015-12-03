4d30371a43ab8592e770504d8968bb5a1efef7c2
huangyibiao <huangyibiao@huangyibiaodeMacBook-Pro.local>
2015-12-03 22:55:02 +0800

    add projects

---
+++ TestApp1/TestApp1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp1
+//
+//  Created by huangyibiao on 15/12/3.
+//  Copyright © 2015年 huangyibiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestApp1/TestApp1Tests/TestApp1Tests.m
@@ -0,0 +1,39 @@
+//
+//  TestApp1Tests.m
+//  TestApp1Tests
+//
+//  Created by huangyibiao on 15/12/3.
+//  Copyright © 2015年 huangyibiao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestApp1Tests : XCTestCase
+
+@end
+
+@implementation TestApp1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
